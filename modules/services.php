dd6fa632c463f7ad57f11914cad5d09e3e4f4202
iubantot <iubantot@gmaill.com>
2016-09-15 21:52:33 +0800

    Fixed File locations

--- modules/services.php
+++ modules/services.php
@@ -8,14 +8,14 @@
     <div class="col-md-8 col-sm-12 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#1">
-          <img src="http://localhost/bes%20v1.0/img/portfolio/1.jpg">
+          <img src="http://localhost/BES/img/portfolio/1.jpg">
         </a>
         <div class="modal fade" id="1" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="http://localhost/bes%20v1.0/img/portfolio/1.jpg">
+                <img src="http://localhost/BES/img/portfolio/1.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>This is the first one on my Gallery</h4>
@@ -28,14 +28,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#2">
-          <img src="http://localhost/bes%20v1.0/img/portfolio/2.jpg">
+          <img src="http://localhost/BES/img/portfolio/2.jpg">
         </a>
         <div class="modal fade" id="2" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="http://localhost/bes%20v1.0/img/portfolio/2.jpg">
+                <img src="http://localhost/BES/img/portfolio/2.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>This is the second one on my Gallery</h4>
@@ -48,14 +48,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#3">
-          <img src="http://localhost/bes%20v1.0/img/portfolio/3.jpg">
+          <img src="http://localhost/BES/img/portfolio/3.jpg">
         </a>
         <div class="modal fade" id="3" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="http://localhost/bes%20v1.0/img/portfolio/3.jpg">
+                <img src="http://localhost/BES/img/portfolio/3.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>This is the third one on my Gallery</h4>
@@ -68,14 +68,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#4">
-          <img src="http://localhost/bes%20v1.0/img/portfolio/4.jpg">
+          <img src="http://localhost/BES/img/portfolio/4.jpg">
         </a>
         <div class="modal fade" id="4" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="http://localhost/bes%20v1.0/img/portfolio/4.jpg">
+                <img src="http://localhost/BES/img/portfolio/4.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>This is the fourth one on my Gallery</h4>
@@ -88,14 +88,14 @@
     <div class="col-md-4 col-sm-6 co-xs-12 gal-item">
       <div class="box">
         <a href="#" data-toggle="modal" data-target="#5">
-          <img src="http://localhost/bes%20v1.0/img/portfolio/4.jpg">
+          <img src="http://localhost/BES/img/portfolio/4.jpg">
         </a>
         <div class="modal fade" id="5" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
               <div class="modal-body">
-                <img src="http://localhost/bes%20v1.0/img/portfolio/4.jpg">
+                <img src="http://localhost/BES/img/portfolio/4.jpg">
               </div>
                 <div class="col-md-12 description">
                   <h4>This is the fifth one on my Gallery</h4>
