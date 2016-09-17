f67f0f7f4c0429d76e2d82e373ffa7b26a7c1e2b
paul028 <paulvincentnonat@gmail.com>
2016-09-17 23:30:45 +0800

    Updated Registration

--- modules/sql-connect/register.php
+++ modules/sql-connect/register.php
@@ -14,26 +14,35 @@ if(isset($_POST['register']))
 	$address=secure($_POST['address'], $mysqli);
 	echo $date;
 	$formatted_date = date('Y-m-d', strtotime($date));
-	$sql = "INSERT INTO users (title_id,username,user_pass,gender,email,birthdate,fname,lname,address)
-	VALUES ('1','$userName','$password','$gender','$eMail','$formatted_date','$firstName','$lastName','$address')";
-
-if ($mysqli->query($sql) === TRUE) {
-   echo    "<script>
-                            alert('Account Successfully Created!!');
-                            window.location.href='../../index.php';
-                        </script>";
 	
-} else {  
-               echo    "<script>
+$q = "SELECT username,email FROM users WHERE username = '$userName' OR email = '$eMail'";
+	if($res = $mysqli->query($q))
+	{
+		if($res->num_rows > 0)
+		{
+			echo    "<script>
                             alert('Username already in Use');
                             window.location.href='../../index.php';
                         </script>";
-			exit;
+		}
+		else
+		{
+			$sql = "INSERT INTO users (title_id,username,user_pass,gender,email,birthdate,fname,lname,address)
+			VALUES ('1','$userName','$password','$gender','$eMail','$formatted_date','$firstName','$lastName','$address')";
+							if ($mysqli->query($sql) === TRUE) {
+			   echo    "<script>
+										alert('Account Successfully Created!!');
+										window.location.href='../../index.php';
+									</script>";
+				
+			} else {  
+						   echo    "<script>
+										alert('Username already in Use');
+										window.location.href='../../index.php';
+									</script>";
+						exit;
+					}
+		}
+	}
 }
-}
-
-
-
-
-
 ?>
